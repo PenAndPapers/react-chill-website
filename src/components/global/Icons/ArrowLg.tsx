8cf2143ecb89fdf1b210280c4bc6aeb83f0075d9
Steven <steven.pilagan@habitap.app>
2022-06-25 17:16:18 +0800

    main - added animation to what we do section

--- src/components/global/Icons/ArrowLg.tsx
+++ src/components/global/Icons/ArrowLg.tsx
@@ -1,11 +1,12 @@
-const ArrowLg = () => {
-  return (
-    <svg width="219" height="22" viewBox="0 0 219 22" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect y="10" width="217" height="2" fill="#17151D"/>
-    <path d="M208 1C208 1 209 4 212 7C215 10 218 11 218 11" stroke="#17151D" strokeWidth="2"/>
-    <path d="M208 21C208 21 209 18 212 15C215 12 218 11 218 11" stroke="#17151D" strokeWidth="2"/>
+import React from 'react'
+
+const ArrowLg = React.forwardRef((props, ref) => (
+    <svg ref={ref} width="219" height="22" viewBox="0 0 219 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <rect y="10" width="217" height="2" fill="#17151D"/>
+      <path d="M208 1C208 1 209 4 212 7C215 10 218 11 218 11" stroke="#17151D" strokeWidth="2"/>
+      <path d="M208 21C208 21 209 18 212 15C215 12 218 11 218 11" stroke="#17151D" strokeWidth="2"/>
     </svg>
   )
-}
+)
 
 export default ArrowLg
--- src/pages/home/components/WhatWeDo/index.tsx
+++ src/pages/home/components/WhatWeDo/index.tsx
@@ -1,23 +1,132 @@
-import style from './style.module.scss'
+import {useEffect, useRef} from 'react'
+import gsap from 'gsap'
 import ArrowLg from '@/components/global/Icons/ArrowLg'
+import style from './style.module.scss'
+
 
 const WhatWeDo = () => {
+  let img = useRef()
+  let curtain = useRef()
+  let h1 = useRef()
+  let h2 = useRef()
+  let h3 = useRef()
+  let h4 = useRef()
+  let p = useRef()
+  let btn = useRef()
+  let arrow = useRef()
+
+  const handleAnimation = () => {
+    const timeLine = gsap.timeline()
+
+    timeLine.fromTo(
+      [h1],
+      {
+        opacity: 0,
+        x: '10rem',
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: .5,
+        ease: 'bounce'
+      }
+    )
+
+    timeLine.fromTo(
+      [curtain],
+      {
+        width: '100%',
+        duration: 1,
+      },
+      {
+        width: 0,
+        ease: 'easeOut'
+      },
+      '-=.25'
+    )
+
+    timeLine.fromTo(
+      [h2, h3, h4], 
+      {
+        opacity: 0,
+        x: '10rem'
+      }, 
+      {
+        opacity: 1,
+        x: 0,
+        stagger: .25,
+        ease: 'bounce'
+      },
+      '-=.5'
+    )
+
+    timeLine.fromTo(
+      [p], 
+      {
+        opacity: 0,
+        x: '5rem',
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: .5,
+        ease: 'bounce'
+      },
+      '-=1'
+    )
+
+    timeLine.fromTo(
+      [btn], 
+      {
+        opacity: 0,
+        x: '-5rem',
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: .5,
+        ease: 'bounce'
+      },
+      '-=.75'
+    )
+
+    timeLine.fromTo(
+      [arrow], 
+      {
+        opacity: 0,
+        x: '-10rem',
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: .5,
+        ease: 'bounce'
+      },
+      '-=.5'
+    )
+  }
+
+  useEffect(() => {
+    handleAnimation()
+  }, [])
+
   return (
     <div className={style.whatWeDo}>
-      <h2>What we do?</h2>
+      <h2 ref={el => {h1 = el}}>What we do?</h2>
       <div className={style.contents}>
         <div className={style.item1}>
-          <img src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="" />
+          <img ref={el => {img = el}} src="https://images.unsplash.com/photo-1581291518857-4e27b48ff24e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=80" alt="" />
+          <div ref={el => {curtain = el}} className={style.curtain} />
         </div>
         <div className={style.item2}>
-          <h3>Branding</h3>
-          <h3>Web Design</h3>
-          <h3>Development</h3>
+          <h3 ref={el => {h2 = el}}>Branding</h3>
+          <h3 ref={el => {h3 = el}}>Web Design</h3>
+          <h3 ref={el => {h4 = el}}>Development</h3>
 
           <div>
-            <p>See all</p>
-            <button>
-              <ArrowLg />
+            <p ref={el => {p = el}}>See all</p>
+            <button ref={el => {btn = el}}>
+              <ArrowLg ref={el => {arrow = el}} />
             </button>
           </div>
         </div>
